e1f30219a33d3ff1c629a18d17667bff2ca79e82
elmouloudiboutaout <elmouloudiboutaout@gmail.com>
2023-03-13 01:33:06 +0100

    bricol client view poc

---
+++ src/App.tsx
@@ -0,0 +1,66 @@
+import { useMemo } from "react";
+import InfiniteScroll from "react-infinite-scroll-component"
+import { useInfiniteQuery } from "@tanstack/react-query";
+
+import { Card } from "./components/Card"
+
+import { ResponseAPI } from "./interface"
+import Loading from "./components/Loading";
+import './App.css'
+
+const fetcher = (page: number): Promise<ResponseAPI> => fetch(`https://rickandmortyapi.com/api/character/?page=${page}`).then(res => res.json())
+
+const App = () => {
+
+  const { data, error, fetchNextPage, status, hasNextPage } = useInfiniteQuery(
+        ['characters'],
+
+        ({ pageParam = 1 }) => fetcher(pageParam),
+
+        {
+            getNextPageParam: (lastPage: ResponseAPI) => {
+
+                const previousPage = lastPage.info.prev ? +lastPage.info.prev.split('=')[1] : 0
+
+                const currentPage = previousPage + 1;
+
+                if (currentPage === lastPage.info.pages) return false;
+                return currentPage + 1;
+            }
+        }
+    )
+
+    const characters = useMemo(() => data?.pages.reduce((prev, page) => {
+        return {
+            info: page.info,
+            results: [...prev.results, ...page.results]
+        }
+    }), [data])
+
+  if (status === 'loading') return <Loading />
+
+  if (status === 'error') return <h4>Ups!, {`${error}` as string}</h4>
+
+
+  return (
+    <div>
+      <h1 className="title">React Infinite Scroll</h1>
+
+      <InfiniteScroll
+        dataLength={characters ? characters.results.length : 0}
+        next={() => fetchNextPage()}
+        hasMore={!!hasNextPage}
+        loader={<Loading />}
+      >
+        <div className="grid-container">
+          {
+            characters && characters.results.map(character => (
+              <Card key={character.id} character={character} />
+            ))
+          }
+        </div>
+      </InfiniteScroll>
+    </div>
+  )
+}
+export default App
---
+++ src/components/Loading.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const loading = () => {
+
+  return (
+        <div className="container-loading">
+            <div className="spinner"></div>
+            <span>Loading more characters...</span>
+        </div> 
+        )
+}
+
+export default loading
